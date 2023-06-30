1bc35fc8770d2c8694299ba1b089d0f194b9852c
xtam <xtam96@example.com>
2023-06-30 12:27:49 -0500

    cambio para actualizar por primera vez en mongo

--- services/redis.service.ts
+++ services/redis.service.ts
@@ -12,14 +12,24 @@ async function syncInfoMongoDB() {
         const keys = await resRedis.keys('*')
         let tmpKeyInfo: any
         keys.map(async item => {
+            console.log(item)
             const infoKeys = item.split("-")
             const id_xtam = parseInt(infoKeys[0])
-            const date = new Date(infoKeys[1])
+            const date = new Date((infoKeys[1]+"-"+infoKeys[2]+"-"+infoKeys[3]))
+       
             tmpKeyInfo = await resRedis.get(item)
             const recording = JSON.parse(tmpKeyInfo)
             const cameras = recording[0]["cameras"]
-            const response = await recordingsModel.findOneAndUpdate({ id_xtam: id_xtam, date: date }, { $set: { cameras: cameras } })
-            console.log("redis updated  !!", response);
+            try {
+                const response = await recordingsModel.findOneAndUpdate(
+                  { id_xtam: id_xtam, date: date },
+                  { $set: { cameras: cameras } },
+                  { upsert: true, new: true }
+                );
+                console.log("redis updated!!", response);
+              } catch (error) {
+                console.error("Error al actualizar en MongoDB:", error);
+              }              
 
         })
 
